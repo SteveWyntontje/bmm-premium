236541de3a262468050fb144cc6a9e5c0640505c
Robin van Oord <robinjude.vanoord@gmail.com>
2026-05-03 16:50:11 +0200

    robin.vue

---
+++ app/pages/robin.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+const { t } = useI18n();
+setTitle(() => t("track.details.about"));
+  
+function calculateAge(birthDate: string) {
+  birthDate = new Date(birthDate);
+  const dateNow = new Date();
+
+  var years = dateNow.getFullYear() - birthDate.getFullYear();
+
+  if (dateNow.getMonth() < birthDate.getMonth() ||
+  (dateNow.getMonth() === birthDate.getMonth() && dateNow.getDate() < birthDate.getDate())) {
+  years--;
+  }
+
+  return years;
+}
+
+var robinsLeeftijd = calculateAge("04/13/2013");
+
+var petrockleeftijd = calculateAge("04/13/2026");
+
+
+
+
+</script>
+
+<template>
+  <div>
+    <h2 class="pb-4 pt-10 text-[28px] font-extrabold">
+      Robin van oord is very cool:D
+    </h2>
+    <p>
+      hello i am {{ robinsLeeftijd }} years young
+    </p>
+    my pet rock named gary is {{ petrockleeftijd }} years
+    <p/>
+    i live in the netherlands
+    <p/>
+    AD  :  https://share.google/osWN5ePkWjNbOQkeE  <--- :D
+  </div>
+</template>
+
+<style>
+.a:link {
+  color: gold;
+  background-color: transparent;
+  text-decoration: none;
+}
+.a:visited {
+  color: red;
+  background-color: transparent;
+  text-decoration: none;
+}
+.a:hover {
+  color: blue;
+  background-color: transparent;
+  text-decoration: underline;
+}
+.a:active {
+  color: green;
+  background-color: transparent;
+  text-decoration: underline;
+}
+</style>
